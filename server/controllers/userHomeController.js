62db2ff31e7456e75e74d45d1cb7d4898b1eddeb
Nimratjot Singh <76111104+Nimratjotsingh@users.noreply.github.com>
2026-08-27 02:10:00 +0530

    change

--- server/controllers/userHomeController.js
+++ server/controllers/userHomeController.js
@@ -753,7 +753,7 @@ export const getOfferDetails = async (req, res) => {
     
     
     await onOfferClickedHook(offer.merchant_id._id, req.user._id);
-    await trackOfferMetric(offer._id, offer.merchant_id._id, "clicks", req.user._id);
+    await trackOfferMetric(offer._id, offer.merchant_id._id, "clicks",{ userId:req.user._id });
     await trackDailyMetric2(offer.merchant_id._id, "totalViewers",{ userId: req.user?._id});
     await trackDailyMetric2(offer.merchant_id._id, "offerClicks",{ userId: req.user?._id,offerId:offer._id});
 
